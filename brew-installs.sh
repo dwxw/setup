0ccdb01496e4926636cfda5ce6bff4db403a28e8
David White <dwxw@users.noreply.github.com>
2021-05-03 14:51:05 +0100

    Update brew-installs.sh

--- brew-installs.sh
+++ brew-installs.sh
@@ -16,9 +16,6 @@ brew upgrade
 BREW_PREFIX=$(brew --prefix)
 
 #Tools
-brew tap mongodb/brew
-brew install mongodb-community
-
 brew install wget --with-iri
 brew install gmp
 brew install grep
@@ -31,11 +28,11 @@ brew install github/gh/gh
 brew install zsh
 
 #Apps
-brew install --cask dropbox
 brew install --cask firefox
 brew install --cask homebrew/cask-versions/firefox-nightly
 brew install --cask google-chrome
 brew install --cask homebrew/cask-versions/google-chrome-canary
+brew install --cask brave-browser
 brew install --cask iterm2
 brew install --cask notion
 brew install --cask spotify
@@ -43,9 +40,8 @@ brew install --cask vlc
 brew install --cask tower
 brew install --cask visual-studio-code
 brew install --cask zoom
-brew install --cask karabiner-elements
-brew install --cask discord
 brew install --cask slack
+brew install --cask tunnelbear
 
 # Remove outdated versions from the cellar.
 brew cleanup
